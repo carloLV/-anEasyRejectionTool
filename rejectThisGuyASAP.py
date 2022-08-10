9d668e77d5972de8877e625a0a25df27420f53af
Carlo La Viola <carlo.laviola@gmail.com>
2022-08-10 10:37:20 +0200

    - Added chime support for making sounds - Added functions to log in also on not-google servers (to be completed) - Added email module to include mail subject and clean up a bit the code
    TODO
    - Add a dependencies file
    - Excel support to reject many MOFOs at once removing the textbox to be
      included. Just navigate the excel file where to get address and names
      from
    - Other improvements that may come to our mind

--- rejectThisGuyASAP.py
+++ rejectThisGuyASAP.py
@@ -8,7 +8,27 @@ This is a temporary script file.
 
 # Import smtplib for the actual sending function
 import smtplib
+import chime
 from tkinter import *
+from email.message import EmailMessage
+
+chime.theme('zelda')
+
+def gmail_login(user,pwd,msg):
+    server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+    server_ssl.ehlo() # optional, called by login()
+    server_ssl.login(user, pwd)  
+    server_ssl.send_message(msg)
+    server_ssl.close()
+    
+def generic_login(user,pwd,msg):
+    server = smtplib.SMTP("Altro server")
+    ## server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.ehlo()
+    server.starttls()
+    server.login(user, pwd)
+    server.sendmail(FROM, TO, message)
+    server.close()
 
 def send_email(recipient, name):
     with open("authentication.txt") as f:
@@ -20,35 +40,39 @@ def send_email(recipient, name):
     SUBJECT = 'Interview result - hahaha'
 
     # Prepare actual message
-    message = """From: %s\nTo: %s\nSubject: %s\n\n
+    message = """
     Dear %s,\n
     We are sorry to inform you that you were not selected.
     Go ask somebody else to hire somebody with so poor skills.
     
     Best regards
-    """ % (user, ", ".join(TO), SUBJECT, name)
+    """ % (name)
     # Prepare actual message
     # message = """From: %s\nTo: %s\nSubject: %s\n\n%s
     # """ % (user, ", ".join(TO), SUBJECT, name)
     
+    ## Create email type message
+    msg = EmailMessage()
+    msg.set_content(message)
+    msg['Subject'] = 'Interview result - hahaha'
+    msg['From'] = user
+    msg['To'] = TO[0]
+    
     try:
-        # server = smtplib.SMTP("smtp.gmail.com", 587)
-        # server.ehlo()
-        # server.starttls()
-        # server.login(user, pwd)
-        # server.sendmail(FROM, TO, message)
-        # server.close()
-        server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-        server_ssl.ehlo() # optional, called by login()
-        server_ssl.login(user, pwd)  
-        # ssl server doesn't support or need tls, so don't call server_ssl.starttls() 
-        server_ssl.sendmail(user, TO, message)
-        #server_ssl.quit()
-        server_ssl.close()
+        ### GMAIL Server
+        gmail_login(user,pwd,msg)
         print ('successfully sent the mail')
+        chime.success()
     except Exception as e:
-        print ("failed to send mail")
-        raise e
+        try:
+            generic_login(user, pwd, msg)
+            print ('successfully sent the mail')
+            chime.success()
+        except Exception as e:
+            chime.error()
+            print ("failed to send mail")
+            raise e
+        chime.error()
         
 main = Tk()
 main.title("An Easy Rejection Tool, AERT")
@@ -65,6 +89,7 @@ def submit():  # Callback function for SUBMIT Button
     print(f'{text=!r}')
 
 def quit():
+    chime.info()
     main.destroy()
 
 c = Canvas(main, width=width, height=height, highlightthickness=0)
